bcaaf46c05ced54b1b4b1bc8d78a000f9e79c587
Himesh-macbook <pererahimesh39@gmail.com>
2023-09-29 17:23:06 +0530

    Initial Commit

---
+++ Appertizers/AppertizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppertizersApp.swift
+//  Appertizers
+//
+//  Created by Himesh's Mac on 2023-09-29.
+//
+
+import SwiftUI
+
+@main
+struct AppertizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
